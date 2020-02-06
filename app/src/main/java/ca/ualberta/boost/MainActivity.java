7051fc5872b267a8902aeebb75444d02c060d926
Andreea <amuresan@ualberta.ca>
2020-02-06 16:17:25 -0700

    added to .gitignore file

--- app/src/main/java/ca/ualberta/boost/MainActivity.java
+++ app/src/main/java/ca/ualberta/boost/MainActivity.java
@@ -1,14 +1,7 @@
 package ca.ualberta.boost;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
