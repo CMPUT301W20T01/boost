4852870cb243141f4d7831eff7f99620427728ef
Michelle Aubin <phiangda@ualberta.ca>
2020-02-23 19:39:49 -0700

    initial commit

--- app/src/main/java/ca/ualberta/boost/MainActivity.java
+++ app/src/main/java/ca/ualberta/boost/MainActivity.java
@@ -1,6 +1,10 @@
 package ca.ualberta.boost;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.signUpButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoMap();
+            }
+        });
+    }
+
+    public void gotoMap(){
+        Intent intent = new Intent(this, RiderMainPage.class);
+        startActivity(intent);
     }
 }
