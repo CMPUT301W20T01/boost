43fd62e919faa05373f894156b165b37bcea8503
Nhan <nhnguyen@ualberta.ca>
2020-02-25 23:16:33 -0700

    modified QRBuck Generator

--- app/src/main/java/ca/ualberta/boost/MainActivity.java
+++ app/src/main/java/ca/ualberta/boost/MainActivity.java
@@ -1,49 +1,13 @@
 package ca.ualberta.boost;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-//NEED TO IMPLEMENT WHEN A USER HAS PRE-DEFINED AMT OF MONEY
-//GENERATE A QR CODE (done)
-//SCAN QR CODE (in progress)
-//EXTRACT INFO FROM SCAN QR CODE TO USER MONEY
-
 public class MainActivity extends AppCompatActivity {
-    private EditText input;
-    private ImageView qr;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input = findViewById(R.id.input);
-        qr = findViewById(R.id.qr);
-        button = findViewById(R.id.button);
-
-        int total_val = 100;
-        final QRBuck money = new QRBuck(total_val);
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String request_val = input.getText().toString();
-                qr.setImageBitmap(money.generateQRCode(request_val));
-
-            }
-        });
-
-
-    }
-
-    public void displayInsufficientError() {
-        Toast.makeText(this, "Insufficient Amount of Money", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/ca/ualberta/boost/QRActivity.java
+++ app/src/main/java/ca/ualberta/boost/QRActivity.java
@@ -1,4 +1,50 @@
 package ca.ualberta.boost;
 
-public class QRActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//NEED TO IMPLEMENT WHEN A USER HAS PRE-DEFINED AMT OF MONEY
+//GENERATE A QR CODE (done)
+//SCAN QR CODE (in progress)
+//EXTRACT INFO FROM SCAN QR CODE TO USER MONEY
+
+public class QRActivity extends AppCompatActivity {
+    private EditText input;
+    private ImageView qr;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_generator);
+        input = findViewById(R.id.input);
+        qr = findViewById(R.id.qr);
+        button = findViewById(R.id.button);
+
+        int total_val = 100;
+        final QRBuck money = new QRBuck(total_val);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String request_val = input.getText().toString();
+                qr.setImageBitmap(money.generateQRCode(request_val));
+
+            }
+        });
+
+    }
+
+    public void displayInsufficientError() {
+        Toast.makeText(this, "Insufficient Amount of Money", Toast.LENGTH_SHORT).show();
+    }
 }
+
+
