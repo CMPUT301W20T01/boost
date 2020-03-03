3f1365c9113ca6b2b4943117276c608812849bf9
Monica Uwalaka <muwalaka@ualberta.ca>
2020-03-02 17:32:43 -0700

    refined user attributes for SignUpActivity.java and sign_up.xml

--- app/src/main/java/ca/ualberta/boost/SignUpActivity.java
+++ app/src/main/java/ca/ualberta/boost/SignUpActivity.java
@@ -80,7 +80,7 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         });
     }
 
-    // adds user to database 
+    // adds user to database
     private void addUser() {
         if(authenticate()) {
             auth.createUserWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim())
