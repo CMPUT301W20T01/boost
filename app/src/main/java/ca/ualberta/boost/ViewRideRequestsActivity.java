fb5c8bbe92fd27de84874811e247e0201a29397d
alexbali <46386190+alexbali@users.noreply.github.com>
2020-03-10 19:44:58 -0600

    request list for driver

---
+++ app/src/main/java/ca/ualberta/boost/ViewRideRequestsActivity.java
@@ -0,0 +1,14 @@
+package ca.ualberta.boost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewRideRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ride_requests);
+    }
+}
