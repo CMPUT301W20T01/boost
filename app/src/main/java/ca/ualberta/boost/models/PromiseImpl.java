4c797038ae95948ae2861389a43fa84da10bb63f
andremures <32344804+andremures@users.noreply.github.com>
2020-03-13 12:37:35 -0600

    User rider driver (#68)
    * added user classes
    
    * created ride classes
    
    * removed lastname and location from constructors
    
    * added rating and num rates to users
    
    * userstore singleton
    
    * pulling
    
    * changed ignore file
    
    * user type enum
    
    * get user
    
    * tests
    
    * changed UML
    
    * no usertype
    
    * no usertype
    
    * baseFare
    
    * javadocs
    
    Co-authored-by: michelle-aubin <46432749+michelle-aubin@users.noreply.github.com>

--- app/src/main/java/ca/ualberta/boost/models/PromiseImpl.java
+++ app/src/main/java/ca/ualberta/boost/models/PromiseImpl.java
@@ -10,22 +10,34 @@ public class PromiseImpl<TResult> implements Promise<TResult> {
 
     public PromiseImpl() {}
 
+    /**
+     * @param result promised data
+     */
     public void resolve(TResult result) {
         if (resolveCallback != null) {
             resolveCallback.onSuccess(result);
         }
     }
 
+    /**
+     * @param e exception, error occurred
+     */
     public void reject(Exception e) {
         if (rejectCallback != null) {
             rejectCallback.onFailure(e);
         }
     }
 
+    /**
+     * @param func the function to be called when the promise is resolved (the result is realized)
+     */
     public void addOnSuccessListener(OnSuccessListener<TResult> func) {
         resolveCallback = func;
     }
 
+    /**
+     * @param func the function to be called when the promise is rejected (an error occurred)
+     */
     public void addOnFailureListener(OnFailureListener func) {
         rejectCallback = func;
     }
