1da4ef981e94e50d2c408826e74c1a9ac24b3fca
Michelle Aubin <phiangda@ualberta.ca>
2020-03-25 18:06:00 -0600

    signup intent test but doesnt delete user from firebase authentication

--- app/src/androidTest/java/ca/ualberta/boost/SignUpActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/boost/SignUpActivityTest.java
@@ -1,11 +1,14 @@
 package ca.ualberta.boost;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.EditText;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -13,8 +16,13 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import ca.ualberta.boost.stores.UserStore;
+
 public class SignUpActivityTest {
     private Solo solo;
+    private FirebaseFirestore fb;
+    private String TAG = "SignUpIntentTest";
+
     @Rule
     public ActivityTestRule<SignUpActivity> rule =
             new ActivityTestRule<>(SignUpActivity.class, true, true);
@@ -28,6 +36,7 @@ public class SignUpActivityTest {
     @Before
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        fb = FirebaseFirestore.getInstance();
     }
 
     /**
@@ -63,6 +72,24 @@ public class SignUpActivityTest {
      */
     @Test
     public void checkSignUp() {
+        Activity activity = rule.getActivity();
+        solo.enterText((EditText) solo.getView(R.id.sign_up_first_name), "Test Name");
+        solo.enterText((EditText) solo.getView(R.id.sign_up_username), "TestSignUp");
+        solo.enterText((EditText) solo.getView(R.id.sign_up_email), "TestSignUp@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.sign_up_phone_number), "7801234567");
+        solo.enterText((EditText) solo.getView(R.id.sign_up_password), "testPassword");
+        solo.clickOnButton("Sign Up");
+        solo.sleep(3000);
+        solo.assertCurrentActivity("Wrong Activity", RiderMainPage.class);
+        // delete the test user
+        fb.collection("users").document("TestSignUp")
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "user deleted");
+                    }
+                });
 
     }
 
