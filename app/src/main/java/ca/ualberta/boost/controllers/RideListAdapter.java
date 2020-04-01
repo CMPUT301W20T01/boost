8ed25e96e4fe3f8b7f38123ff121a24a71d9add7
Matthew B <buddymjb@hotmail.com>
2020-03-31 21:33:06 -0600

    Updated Ride History

--- app/src/main/java/ca/ualberta/boost/controllers/RideListAdapter.java
+++ app/src/main/java/ca/ualberta/boost/controllers/RideListAdapter.java
@@ -1,4 +1,43 @@
 package ca.ualberta.boost.controllers;
 
-public class RideListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import ca.ualberta.boost.R;
+import ca.ualberta.boost.models.Ride;
+
+public class RideListAdapter extends ArrayAdapter<Ride> {
+
+    public RideListAdapter(@NonNull Context context, ArrayList<Ride> rides) {
+        super(context, R.layout.ride_details, rides);
+    }
+
+    @Override
+    public View getView (int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View customView = inflater.inflate(R.layout.ride_details, parent, false);
+
+        Ride individualRide = getItem(position);
+        TextView driverName = customView.findViewById(R.id.rideDetailsDriver);
+        TextView riderName = customView.findViewById(R.id.rideDetailsRider);
+        TextView startLoc = customView.findViewById(R.id.rideDetailsPickup);
+        TextView endLoc = customView.findViewById(R.id.rideDetailsDropoff);
+
+        driverName.setText(individualRide.getDriverUsername());
+        riderName.setText(individualRide.getRiderUsername());
+        startLoc.setText(individualRide.getStartLocation().toString());
+        endLoc.setText(individualRide.getEndLocation().toString());
+
+        return customView;
+    }
 }
