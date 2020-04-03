ed29b4a873f21456fbcefcc5b4c5441dba3921cb
alexbali <46386190+alexbali@users.noreply.github.com>
2020-04-03 15:47:21 -0600

    email and phone tests (#139)

--- app/src/androidTest/java/ca/ualberta/boost/CallActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/boost/CallActivityTest.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.firebase.ui.auth.ui.phone.PhoneActivity;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -21,9 +22,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import java.util.Random;
 
-import ca.ualberta.boost.stores.UserStore;
+/**
+ * Tests for making sure the user profile can call and email
+ */
 
-public class SignUpActivityTest {
+public class CallActivityTest {
+
     private Solo solo;
     private FirebaseFirestore fb;
     private FirebaseAuth auth;
@@ -56,29 +60,12 @@ public class SignUpActivityTest {
         Activity activity = rule.getActivity();
     }
 
-    /**
-     * Tests that user cannot sign up with a non unique username
-     */
-    @Test
-    public void checkUniqueUsername() {
-        Activity activity = rule.getActivity();
-        solo.enterText((EditText) solo.getView(R.id.sign_up_first_name), "Test Name");
-        // riderMichelle is already taken
-        solo.enterText((EditText) solo.getView(R.id.sign_up_username), "riderMichelle");
-        solo.enterText((EditText) solo.getView(R.id.sign_up_email), "testingtesting@gmail.com");
-        solo.enterText((EditText) solo.getView(R.id.sign_up_phone_number), "7809999999");
-        solo.enterText((EditText) solo.getView(R.id.sign_up_password), "testPassword");
-        solo.clickOnButton("Sign Up");
-        solo.searchText("Username is taken");
-        // signup should fail and remain on signup activity
-        solo.assertCurrentActivity("Wrong Activity", SignUpActivity.class);
-    }
 
     /**
-     * Tests that a rider can successfully sign up with unique information
+     * test to make sure that the phone activity opens with correct phone number
      */
     @Test
-    public void signupAsRider(){
+    public void testCallPhoneNumber(){
         Activity activity = rule.getActivity();
         Random rand = new Random();
         int randomNum = rand.nextInt(1000000);
@@ -98,16 +85,33 @@ public class SignUpActivityTest {
 //        ImageView image = (ImageView) solo.getView(R.id.call_icon_private);
         solo.clickOnText("7809999999");
         solo.sleep(4000);
+        solo.assertCurrentActivity("correct", CallActivity.class);
     }
 
-
-
     /**
-     * Close activity after each test
-     * @throws Exception
+     * test to make sure that the email activity opens with correct information
      */
-    @After
-    public void tearDown() throws Exception{
-        solo.finishOpenedActivities();
+    @Test
+    public void testUserProfileEmail(){
+        Activity activity = rule.getActivity();
+        Random rand = new Random();
+        int randomNum = rand.nextInt(1000000);
+        solo.enterText((EditText) solo.getView(R.id.sign_up_first_name), "Test Name");
+        // riderMichelle is already taken
+        solo.enterText((EditText) solo.getView(R.id.sign_up_username), "testRider"+randomNum);
+        solo.enterText((EditText) solo.getView(R.id.sign_up_email), "testRider"+randomNum+"@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.sign_up_phone_number), "7809999999");
+        solo.enterText((EditText) solo.getView(R.id.sign_up_password), "testPassword");
+        solo.clickOnButton("Sign Up");
+//        solo.searchText("Username is taken");
+        // signup should fail and remain on signup activity
+        solo.sleep(5000);
+        solo.assertCurrentActivity("Correct Activity", RiderMainPage.class);
+        solo.clickOnButton("Profile");
+        solo.sleep(5000);
+//        ImageView image = (ImageView) solo.getView(R.id.call_icon_private);
+        solo.clickOnText("testRider"+randomNum+"@gmail.com");
+        solo.sleep(4000);
+        solo.assertCurrentActivity("correct",EmailActivity.class);
     }
 }
